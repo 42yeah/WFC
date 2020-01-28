11571c1355c44b58e7b4eb10e796d7b929e1cdb5
42yeah <potion@live.cn>
2020-01-28 12:24:35 +0800

    Initial Commit

---
+++ WFC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WFC
+//
+//  Created by 周昊 on 2020/1/28.
+//  Copyright © 2020 aiofwa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
